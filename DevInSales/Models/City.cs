bee97dfbe00110e8d88fa1bdeb0c06a6ff9e9e63
Vinicius Hiago da Rosa <93688147+viniHiagoRosa@users.noreply.github.com>
2022-04-26 20:56:15 -0300

    feat: create class city

---
+++ DevInSales/Models/City.cs
@@ -0,0 +1,9 @@
+﻿namespace DevInSales.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int State_Id { get; set; }
+    }
+}
