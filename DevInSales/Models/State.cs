05ef3360e53337366ec29505662a489c7428d22d
Vinicius Hiago da Rosa <93688147+viniHiagoRosa@users.noreply.github.com>
2022-04-26 20:51:53 -0300

    feat: create class state

---
+++ DevInSales/Models/State.cs
@@ -0,0 +1,9 @@
+﻿namespace DevInSales.Models
+{
+    public class State
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Initials { get; set; }
+    }
+}
