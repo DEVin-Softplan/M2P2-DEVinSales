80a51026f8112c6fdc2110d28de667662bf0a932
Matheus Moreira <matheus_s_moreira@estudante.sesisenai.org.br>
2022-04-27 16:28:54 -0300

    Criado Entidade order_product contexto atualizado e migration.

--- DevInSales/Context/SqlContext.cs
+++ DevInSales/Context/SqlContext.cs
@@ -8,4 +8,5 @@ public class SqlContext : DbContext
     public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
     public DbSet<User> User { get; set; }
     public DbSet<Profile> Profile { get; set; }
+    public DbSet<order_product> order_product { get; set; }
 }
---
+++ DevInSales/Migrations/20220427190328_AdicionadoOrderProduct.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevInSales.Migrations
+{
+    public partial class AdicionadoOrderProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "order_product",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    order_id = table.Column<int>(type: "int", nullable: false),
+                    product_id = table.Column<int>(type: "int", nullable: false),
+                    unit_price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_order_product", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "order_product");
+        }
+    }
+}
