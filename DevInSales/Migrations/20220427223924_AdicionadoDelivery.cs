ca470ff7242bffba800280217768ad9cb126486c
Narriman Lima <narrimancslima@gmail.com>
2022-04-27 19:45:07 -0300

    Feat: Criada entidade delivery, alterado tipo do unit_price, realizada migration e atualização do context

---
+++ DevInSales/Migrations/20220427223924_AdicionadoDelivery.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevInSales.Migrations
+{
+    public partial class AdicionadoDelivery : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "unit_price",
+                table: "order_product",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.CreateTable(
+                name: "delivery",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    order_id = table.Column<int>(type: "int", nullable: false),
+                    address_id = table.Column<int>(type: "int", nullable: false),
+                    delivery_forecast = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    delivery_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_delivery", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "delivery");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "unit_price",
+                table: "order_product",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
--- DevInSales/Models/order_product.cs
+++ DevInSales/Models/order_product.cs
@@ -8,7 +8,7 @@ public class order_product
     public int id { get; set; }
     public int order_id { get; set; }
     public int product_id { get; set; }
-    public decimal unit_price { get; set; }
+    public float unit_price { get; set; }
     public int amount { get; set; }
     
 }
