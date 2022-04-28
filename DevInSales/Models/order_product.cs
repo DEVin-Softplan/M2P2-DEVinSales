dda4cd2a6e470fdaee768eb3cec1bcc23b1bd30a
Matheus Moreira <matheus_s_moreira@estudante.sesisenai.org.br>
2022-04-28 19:57:52 -0300

    Correção Classe

--- DevInSales/Models/order_product.cs
+++ DevInSales/Models/order_product.cs
@@ -6,7 +6,7 @@ namespace DevInSales.Models;
 public class Order_Product
 {
     public int Id { get; set; }
-    public Order? Order_Id { get; set; }
+    public Order Order_Id { get; set; }
     public int Product_Id { get; set; }
     public float Unit_Price { get; set; }
     public int Amount { get; set; }
