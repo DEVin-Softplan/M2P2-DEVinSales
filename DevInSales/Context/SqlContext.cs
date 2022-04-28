d3831cb6fe8d96b03dee3e4a9be6e0b8f16766a8
VeroniJrStudant <veroni_vieira-jun1@estudante.sesisenai.org.br>
2022-04-27 22:14:17 -0300

    Criado as variaveis state, city e address no SqlContext

--- DevInSales/Context/SqlContext.cs
+++ DevInSales/Context/SqlContext.cs
@@ -20,11 +20,34 @@ public class SqlContext : DbContext
         product.Property(x => x.Name).HasColumnName("name").HasColumnType("varchar(100)").IsRequired();
         product.Property(x => x.Suggested_Price).HasColumnName("suggested_price").HasColumnType("decimal").IsRequired();
 
+
         var category = modelBuilder.Entity<Category>();
         category.HasKey(x => x.Id);
         category.Property(x => x.Id).HasColumnName("id").ValueGeneratedOnAdd();
         category.Property(x => x.Name).HasColumnName("name").HasColumnType("varchar(100)").IsRequired();
         category.Property(x => x.Slug).HasColumnName("slug").HasColumnType("varchar(100)").IsRequired();
+
+        var state = modelBuilder.Entity<State>();
+        state.HasKey(x => x.Id);
+        state.Property(x => x.Id).HasColumnName("id").ValueGeneratedOnAdd();
+        state.Property(x => x.Name).HasColumnName("name").HasColumnType("varchar(100)").IsRequired();
+        state.Property(x => x.Initials).HasColumnName("initials").HasColumnType("varchar(2)").IsRequired();
+
+        var city = modelBuilder.Entity<City>();
+        city.HasKey(x => x.Id);
+        city.Property(x => x.Id).HasColumnName("id").ValueGeneratedOnAdd();
+        city.Property(x => x.Name).HasColumnName("name").HasColumnType("varchar(100)").IsRequired();
+        city.Property(x => x.State_Id).HasColumnName("state_id").HasColumnType("int").IsRequired();
+        
+        var address = modelBuilder.Entity<Address>();
+        address.HasKey(x => x.Id);
+        address.Property(x => x.Id).HasColumnName("id").ValueGeneratedOnAdd();
+        address.Property(x => x.Street).HasColumnName("street").HasColumnType("varchar(200)").IsRequired();
+        address.Property(x => x.Complement).HasColumnName("complement").HasColumnType("varchar(200)");
+        address.Property(x => x.CEP).HasColumnName("CEP").HasColumnType("varchar(9)").IsRequired();
+        address.Property(x => x.Number).HasColumnName("number").HasColumnType("int").IsRequired();
+        address.Property(x => x.City_Id).HasColumnName("city_id").HasColumnType("int").IsRequired();
+
 
         modelBuilder.Entity<State>().HasData(
           new State
@@ -96,7 +119,7 @@ public class SqlContext : DbContext
           new State
           {
               Id = 25,
-              Name = "Para�ba",
+              Name = "Paraíba",
               Initials = "PB"
           },
           new State
@@ -144,7 +167,7 @@ public class SqlContext : DbContext
           new State
           {
               Id = 35,
-              Name = "S�o Paulo",
+              Name = "São Paulo",
               Initials = "SP"
           },
           new State
