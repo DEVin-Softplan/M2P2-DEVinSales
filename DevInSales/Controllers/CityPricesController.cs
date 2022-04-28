8f67ca58e07a0bba12e4ebe59400f71f456a50fc
Werik Santos <werik.msti@gmail.com>
2022-04-27 21:23:52 -0300

    feat: Criaçao dos Controllers

---
+++ DevInSales/Controllers/CityPricesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevInSales.Context;
+
+namespace DevInSales.Controllers
+{
+    [Route("api/cityPrices")]
+    [ApiController]
+    public class CityPricesController : ControllerBase
+    {
+        private readonly SqlContext _context;
+
+        public CityPricesController (SqlContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
---
+++ DevInSales/Controllers/StatePricesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevInSales.Context;
+
+namespace DevInSales.Controllers
+{
+    [Route("api/statePrices")]
+    [ApiController]
+    public class StatePricesController : ControllerBase
+    {
+        private readonly SqlContext _context;
+
+        public StatePricesController(SqlContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
