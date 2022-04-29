1a8421d3c4fe53b8eb278abefe47a5860237708f
Narriman Lima <narrimancslima@gmail.com>
2022-04-29 17:48:35 -0300

    Feat: relacionamento entidade Order

--- DevInSales/Models/Order.cs
+++ DevInSales/Models/Order.cs
@@ -3,11 +3,10 @@
     public class Order
     {
         public int Id { get; set; }
-        public int User_Id { get; set; }
-        public int Seller_Id { get; set; }
+        public User User_Id { get; set; }
+        public User Seller_Id { get; set; }
         public DateTime Date_Order { get; set; }
-        public string Shipping_Company { get; set; }
-        public float Shipping_Company_Price { get; set; }
-
+        public ShippingCompany Shipping_Company { get; set; }
+        public CityPrice Shipping_Company_Price { get; set; }
     }
 }
