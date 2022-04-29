1b4af31c608890119076591e7461c2933e47df48
zighue1 <federico_robillard@estudante.sesisenai.org.br>
2022-04-29 20:13:11 -0300

    feat: CitySeed

--- DevInSales/Context/SqlContext.cs
+++ DevInSales/Context/SqlContext.cs
@@ -27,9 +27,8 @@ public class SqlContext : DbContext
         category.Property(x => x.Name).HasColumnName("name").HasColumnType("varchar(100)").IsRequired();
         category.Property(x => x.Slug).HasColumnName("slug").HasColumnType("varchar(100)").IsRequired();
 
-        modelBuilder.Entity<State>().HasData(
-         StateSeed.Seed);
-          
+        modelBuilder.Entity<State>().HasData(StateSeed.Seed);
+        modelBuilder.Entity<City>().HasData(CitySeed.Seed());
     }
 
 }
