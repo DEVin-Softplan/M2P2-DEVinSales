0387c8fc0af272bc7c5143202dad412e93a6b520
Romulo-Ladeira <93687924+Romulo-Ladeira@users.noreply.github.com>
2022-04-29 18:46:42 -0300

    Fix: entity Order_Product

--- DevInSales/Models/order_product.cs
+++ DevInSales/Models/order_product.cs
@@ -6,8 +6,8 @@ namespace DevInSales.Models;
 public class Order_Product
 {
     public int Id { get; set; }
-    public Order Order_Id { get; set; }
-    public Product Product_Id { get; set; }
+    public Order Order { get; set; }
+    public Product Product { get; set; }
     public decimal Unit_Price { get; set; }
     public int Amount { get; set; }
 
