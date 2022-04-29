ee9608faf765c003d39984ebce8d87b80a5310b0
Romulo-Ladeira <93687924+Romulo-Ladeira@users.noreply.github.com>
2022-04-29 18:26:40 -0300

    Fix: entidade Delivery

--- DevInSales/Models/delivery.cs
+++ DevInSales/Models/delivery.cs
@@ -5,8 +5,8 @@ namespace DevInSales.Models
     public class Delivery
     {
         public int Id { get; set; }
-        public Order Order_Id { get; set; }
-        public Address Address_Id { get; set; }
+        public Order Order { get; set; }
+        public Address Address { get; set; }
         public DateTime Delivery_Forecast { get; set; }
         public DateTime Delivery_Date { get; set; }
         public StatusEnum Status { get; set; }
