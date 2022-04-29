8dd264345cbce4782c336adc412da7fa8a66b2e4
Narriman Lima <narrimancslima@gmail.com>
2022-04-29 17:16:25 -0300

    Fix: Entidade Order_Product

--- DevInSales/Models/order_product.cs
+++ DevInSales/Models/order_product.cs
@@ -8,7 +8,8 @@ public class Order_Product
     public int Id { get; set; }
     public Order Order_Id { get; set; }
     public Product Product_Id { get; set; }
-    public float Unit_Price { get; set; }
+    public decimal Unit_Price { get; set; }
     public int Amount { get; set; }
-    
+
+
 }
