7337589040f575afed436d1c3943de207feb4b35
Romulo-Ladeira <93687924+Romulo-Ladeira@users.noreply.github.com>
2022-04-28 21:57:33 -0300

    Feat: Criando StatusEnum e criando relacionamento do Modelo Delivery

---
+++ DevInSales/Enums/StatusEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace DevInSales.Enums
+{
+    public enum StatusEnum
+    {
+        AguardandoAprovacaoDePagamento = 1,
+        PreparandoPacote = 2,
+        PedidoEnviando = 3,
+        PedidoEmTransporte = 4,
+        PedidoEntrege= 5,
+    }
+}
