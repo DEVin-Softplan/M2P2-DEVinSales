4aef1f38453e63e05c292fb05077c64beb17cd3d
Narriman Lima <narrimancslima@gmail.com>
2022-04-29 19:29:51 -0300

    Fix: atributo order

--- DevInSales/Models/Order.cs
+++ DevInSales/Models/Order.cs
@@ -3,8 +3,8 @@
     public class Order
     {
         public int Id { get; set; }
-        public User User_Id { get; set; }
-        public User Seller_Id { get; set; }
+        public User User { get; set; }
+        public User Seller { get; set; }
         public DateTime Date_Order { get; set; }
         public ShippingCompany Shipping_Company { get; set; }
         public CityPrice Shipping_Company_Price { get; set; }
