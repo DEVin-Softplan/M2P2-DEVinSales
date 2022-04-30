53c070035e69671bc73c5a64211bce48058a8a16
VeroniJrStudant <veroni_vieira-jun1@estudante.sesisenai.org.br>
2022-04-30 14:32:12 -0300

    Criado o EndPoint Get state/{State_Id}/city/{City_Id}

---
+++ DevInSales/DTOs/CityStateDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace DevInSales.DTOs
+{
+    public class CityStateDTO
+    {
+        public int City_Id { get; set; }
+        public string Name_City { get; set; }
+
+        public int State_Id { get; set; }
+        public string Name_State { get; set; }
+        public string Initials { get; set; }
+    }
+}
