ea565dd80e62377c92cd22b51733bb45e799ee65
Laci Leal Junior <93396326+LaciJr@users.noreply.github.com>
2022-04-29 21:01:56 -0300

    fix: Column id

--- DevInSales/Models/Profile.cs
+++ DevInSales/Models/Profile.cs
@@ -4,7 +4,7 @@ namespace DevInSales.Models;
 
 public class Profile
 {
-    [Column("Id")]
+    [Column("id")]
     public int Id { get; set; }
     [Column("name")]
     public string Name{ get; set; }
