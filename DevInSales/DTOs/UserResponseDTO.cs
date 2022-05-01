07f9cc8392ce629f669b8119189b9da5a11beb21
Camila Kunitz <camila.kunitz@gmail.com>
2022-05-01 15:50:11 -0300

    Cria DTO de resposta para busca de usuários

---
+++ DevInSales/DTOs/UserResponseDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevInSales.DTOs
+{
+    public class UserResponseDTO
+    {
+        [Display(Name = "id")]
+        public int Id { get; set; }
+
+        [Display(Name = "name")]
+        public string Name { get; set; }
+
+        [Display(Name = "email")]
+        public string Email { get; set; }
+
+        [Display(Name = "birthDate")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+    }
+}
