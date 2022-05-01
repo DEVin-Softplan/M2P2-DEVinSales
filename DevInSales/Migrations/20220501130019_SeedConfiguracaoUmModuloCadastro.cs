a24d5e335225e7329f1d3e0d60aefdc418667d28
Vinícius Souza <vinigsouza.bnu@gmail.com>
2022-05-01 10:23:21 -0300

    Inserindo as seeds de categoria e perfil da configuração 1

---
+++ DevInSales/Migrations/20220501130019_SeedConfiguracaoUmModuloCadastro.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevInSales.Migrations
+{
+    public partial class SeedConfiguracaoUmModuloCadastro : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "id", "name", "slug" },
+                values: new object[] { 1, "Categoria Padrão", "categoria-padrao" });
+
+            migrationBuilder.InsertData(
+                table: "Profile",
+                columns: new[] { "id", "name" },
+                values: new object[] { 1, "Cliente" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Profile",
+                keyColumn: "id",
+                keyValue: 1);
+        }
+    }
+}
--- DevInSales/Models/Category.cs
+++ DevInSales/Models/Category.cs
@@ -6,5 +6,26 @@
         public string Name { get; set; }
         public string Slug { get; set; }
         public ICollection<Product> Product { get; set; }
+
+        public Category()
+        {
+        }
+
+        public Category(string name, string slug)
+        {
+            Name = name;
+            Slug = slug;
+        }
+        public Category(int id, string name, string slug)
+        {
+            Id = id;
+            Name = name;
+            Slug = slug;
+        }
+
+        public static string GetCategorySlug(string name)
+        {
+            return name.ToLower().Replace(" ", "-").Normalize();
+        }
     }
 }
