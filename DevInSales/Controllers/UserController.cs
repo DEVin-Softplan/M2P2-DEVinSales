22769c21d2e4fd94212eee06a34d21e7f514c17e
Camila Kunitz <camila.kunitz@gmail.com>
2022-05-01 11:33:17 -0300

    Adiciona validação para idade do usuário
    Adicionado validação para validadar cadastro somente de usuários maiores de 18 anos.

--- DevInSales/Controllers/UserController.cs
+++ DevInSales/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using DevInSales.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 namespace DevInSales.Controllers
 {
@@ -9,9 +10,26 @@ namespace DevInSales.Controllers
     {
 
         [HttpPost]
-        public UserPostDTO Create([FromBody] UserPostDTO user)
+        public async Task<ActionResult<UserPostDTO>> Create([FromBody] UserPostDTO userDTO)
         {
-            return user;
+            if (!isDataNascimentoValida(userDTO.BirthDate))
+            {
+                return BadRequest("O usuário deve ser maior de 18 anos.");
+            }
+
+            return Ok(userDTO);
+        }
+
+        private bool isDataNascimentoValida(string data)
+        {
+            DateTime dataNascimento = DateTime.ParseExact(data, "dd/MM/yyyy", new CultureInfo("pt-BR"));
+            DateTime diaAtual = DateTime.Today;
+            int idade = diaAtual.Year - dataNascimento.Year;
+            if (idade > 18)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
