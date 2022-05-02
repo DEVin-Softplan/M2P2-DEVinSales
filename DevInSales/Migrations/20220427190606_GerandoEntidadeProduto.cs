79dd57f8bb3cc42cb65267e16f17e7bb82999475
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-05-02 13:30:53 -0300

    hotfix: Adiciona migrations da main para resolver conflitos

---
+++ DevInSales/Migrations/20220427190606_GerandoEntidadeProduto.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevInSales.Migrations
+{
+    public partial class GerandoEntidadeProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    suggested_price = table.Column<decimal>(type: "decimal", nullable: false),
+                    category = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
