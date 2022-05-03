9efab4b9a862fe2b14c3cff1e30d55e96c7028f5
Kazyg <guilherme_severo@estudante.sc.senai.br>
2022-05-03 03:23:22 -0300

    Adicionando PatchProduct
    patch para atualizar Nome e preço sugerido

--- DevInSales/Controllers/ProductController.cs
+++ DevInSales/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevInSales.Context;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -6,7 +8,58 @@ namespace DevInSales.Controllers
 {
     [Route("api/product")]
     [ApiController]
+
     public class ProductController : ControllerBase
-    {
-    }
-}
+        {
+            private readonly SqlContext _context;
+
+            public ProductController(SqlContext context)
+            {
+                _context = context;
+            }
+
+        
+
+        /// <summary>
+        ///atualiza o nome e preço do produto
+        /// </summary>
+        /// <param name="productModel"></param>
+        /// <param name="id"></param>
+        /// <returns>atualiza o nome e preço do produto</returns>
+        /// <response code="200">Produto Atualizado</response>
+        /// <response code="204">Sem nenhum retorno.</response>
+        /// <response code="400">Erro ao fazer a Request.</response>
+        /// <response code="404">produto inexistente</response>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpPatch("{id}")]
+            public async Task<IActionResult> PatchProduct(JsonPatchDocument productModel, int id)
+            {
+
+            var product = await _context.Product.FindAsync(id);
+
+            if(product == null)
+            {
+                return StatusCode(404);
+            }
+ 
+            if(product != null)
+            {
+                productModel.ApplyTo(product);
+                if (string.IsNullOrWhiteSpace(product.Name))
+                {
+                    return StatusCode(404);
+                }
+                if(product.Suggested_Price <= 0)
+                {
+                    return StatusCode(404);
+                }
+                await _context.SaveChangesAsync();
+                return StatusCode(204);
+            }
+            return StatusCode(200);
+        }
+        }
+    } 
---
+++ DevInSales/DTOs/ProductGetDTO.cs
@@ -0,0 +1,22 @@
+﻿using DevInSales.Models;
+
+namespace DevInSales.DTOs;
+public class ProductGetDTO
+{
+    public string Name { get; set; }
+    public decimal Suggested_Price { get; set; }
+    public string Category_Name { get; set; }
+
+    public static explicit operator ProductGetDTO(Product product)
+    {
+        if (product == null)
+            return null;
+
+        return new ProductGetDTO()
+        {
+            Name = product.Name,
+            Suggested_Price = product.Suggested_Price,
+            Category_Name = product.Category.Name
+        };
+    }
+}
