fafa3af1d818162bba147b98cf998a987c239e12
douglas-devinhouse <douglas_nascimento4@estudante.sesisenai.org.br>
2022-05-03 16:41:47 -0700

    feat(update): populando seeds.

--- DevInSales/Seeds/CityPriceSeed.cs
+++ DevInSales/Seeds/CityPriceSeed.cs
@@ -16,16 +16,58 @@ namespace DevInSales.Seeds
             new CityPrice
             {
                 Id = 2,
-                CityId = 2,
+                CityId = 1,
                 ShippingCompanyId = 2,
                 BasePrice = 20
             },
             new CityPrice
             {
                 Id = 3,
-                CityId = 3,
+                CityId = 1,
                 ShippingCompanyId = 3,
                 BasePrice = 30
+            },
+            new CityPrice
+            {
+                Id = 4,
+                CityId = 2,
+                ShippingCompanyId = 1,
+                BasePrice = 21
+            },
+            new CityPrice
+            {
+                Id = 5,
+                CityId = 2,
+                ShippingCompanyId = 2,
+                BasePrice = 22
+            },
+            new CityPrice
+            {
+                Id = 6,
+                CityId = 2,
+                ShippingCompanyId = 3,
+                BasePrice = 23
+            },
+            new CityPrice
+            {
+                Id = 7,
+                CityId = 3,
+                ShippingCompanyId = 1,
+                BasePrice = 31
+            },
+            new CityPrice
+            {
+                Id = 8,
+                CityId = 3,
+                ShippingCompanyId = 2,
+                BasePrice = 32
+            },
+            new CityPrice
+            {
+                Id = 9,
+                CityId = 3,
+                ShippingCompanyId = 3,
+                BasePrice = 33
             },
         };
     }
