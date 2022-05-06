0c50dcb2a9a7ecf3d0725c84494bbc992d278c13
Camila Kunitz <camila.kunitz@gmail.com>
2022-05-06 20:03:01 -0300

    Ajusta validação de maioridade para considerar meses e dias

--- DevInSales/Controllers/UserController.cs
+++ DevInSales/Controllers/UserController.cs
@@ -135,6 +135,10 @@ namespace DevInSales.Controllers
             DateTime dataNascimento = DateTime.ParseExact(data, "dd/MM/yyyy", new CultureInfo("pt-BR"));
             DateTime diaAtual = DateTime.Today;
             int idade = diaAtual.Year - dataNascimento.Year;
+            if (dataNascimento > diaAtual.AddYears(-idade))
+            {
+                idade--;
+            }
             if (idade >= 18)
             {
                 return true;
