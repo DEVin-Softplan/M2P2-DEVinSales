97c804a5a198b04f34d57e7e51b891ab7eade830
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-05-08 15:10:51 -0300

    feat: Adiciona Endpoint 8 Post Delivery

--- DevInSales/Controllers/DeliveryController.cs
+++ DevInSales/Controllers/DeliveryController.cs
@@ -1,6 +1,5 @@
 ﻿using DevInSales.Context;
 using DevInSales.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,7 +9,7 @@ namespace DevInSales.Controllers
     [ApiController]
     public class DeliveryController : ControllerBase
     {
-        
+
         private readonly SqlContext _context;
 
         public DeliveryController(SqlContext context)
@@ -34,8 +33,6 @@ namespace DevInSales.Controllers
         public async Task<ActionResult<ICollection<Delivery>>> GetDelivery(int address_id, int order_id)
         {
             var deliverys = _context.Delivery.Include(x => x.Order).Include(x => x.Address).ToList();
-                
-
 
             if (address_id == 0 && order_id == 0)
             {
@@ -58,7 +55,7 @@ namespace DevInSales.Controllers
             {
                 return NoContent();
             }
-            return Ok(deliverys); 
+            return Ok(deliverys);
         }
 
         /// <summary>
@@ -104,5 +101,54 @@ namespace DevInSales.Controllers
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="order_id"></param>
+        /// <param name="address_id"></param>
+        /// <returns></returns>
+        /// <response code="200"></response>
+        /// <response code="404"></response>
+        /// <response code="400"></response>
+        /// <response code="500"></response>
+        [HttpPost("order/{order_id}/delivery")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> PostDelivery(int order_id, int address_id, DateTime delivery_forecast)
+        {
+            try
+            {
+                var orderDB = await _context.Order.FindAsync(order_id);
+                var addressDB = await _context.Address.FindAsync(address_id);
+
+                if (orderDB == null || addressDB == null)
+                {
+                    return StatusCode(400);
+                }
+
+                if (address_id.ToString() == null)
+                {
+                    return StatusCode(400);
+                }
+
+                if (delivery_forecast < DateTime.Now)
+                {
+                    return StatusCode(400);
+                }
+
+                var delivery = new Delivery { Address = addressDB, Delivery_Date = null, Delivery_Forecast = delivery_forecast, Order = orderDB, Status = Enums.StatusEnum.PedidoEmTransporte };
+                _context.Delivery.Add(delivery);
+                await _context.SaveChangesAsync();
+
+                return StatusCode(201);
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
     }
 }
