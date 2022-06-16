a68e5125e4378991243d1c4898340309fbefb228
Vinícius <vinigsouza.bnu@gmail.com>
2022-06-15 23:21:46 -0300

    Inclusão do projeto de testes + método para gerar token

---
+++ DevInSales.Test/utils/TokenGenerator.cs
@@ -0,0 +1,25 @@
+﻿using DevInSales.Models;
+using DevInSales.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevInSales.Test.utils
+{
+    public class TokenGenerator
+    {
+        public static string GenerateToken(EProfileType tokenType)
+        {
+            var profile = new Profile() { Type = tokenType };
+            var user = new User()
+            {
+                Id = 1,
+                Name = "Testes",
+                Profile = profile,
+            };
+            return new TokenService().GenerateToken(user);
+        }
+    }
+}
