e30a10f7f980105e5eae14ebd4c01e89d0c813cc
Vinícius <vinigsouza.bnu@gmail.com>
2022-06-18 15:22:13 -0300

    Criação do teste de permissionamento get - order

---
+++ DevInSales.Test/OrderTest/OrderIntegrationTest.cs
@@ -0,0 +1,70 @@
+﻿using DevInSales.Test.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevInSales.Test.OrderTest
+{
+    public class OrderIntegrationTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task RetornoEndPoint()
+        {
+            OrderFactory factory = new OrderFactory();
+
+            // given
+
+            var client = factory.CreateClient();
+
+            // when
+
+            var response = await client.GetAsync("/api/order/user/1/order");
+
+            // then
+
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+        }
+
+        [Test]
+        public async Task PermissionamentoGet()
+        {
+            OrderFactory factory = new OrderFactory();
+            var client = factory.CreateClient();
+            var clientAdmin = factory.CreateClient();
+            var clientGerente = factory.CreateClient();
+            var clientUsuario = factory.CreateClient();
+
+            // given
+
+            var tokenAdmin = TokenGenerator.GenerateToken(Security.EProfileType.Admin);
+            var tokenGerente = TokenGenerator.GenerateToken(Security.EProfileType.Gerente);
+            var tokenUsuario = TokenGenerator.GenerateToken(Security.EProfileType.Usuario);
+
+            clientAdmin.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenAdmin);
+            clientGerente.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenGerente);
+            clientUsuario.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenUsuario);
+
+            // when
+
+            var response = await client.GetAsync("/api/order/user/1/order");
+            var responseAdmin = await clientAdmin.GetAsync("/api/order/user/1/order");
+            var responseGerente = await clientGerente.GetAsync("/api/order/user/1/order");
+            var responseUsuario = await clientUsuario.GetAsync("/api/order/user/1/order");
+
+            // then
+
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+            Assert.That(responseAdmin.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+            Assert.That(responseGerente.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+            Assert.That(responseUsuario.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+        }
+    }
+}
