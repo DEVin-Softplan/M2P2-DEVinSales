3693e64fefb06618b22ce713ce33fc6a612e9e3d
Vinícius <vinigsouza.bnu@gmail.com>
2022-06-18 15:21:52 -0300

    Criação da factory - order

---
+++ DevInSales.Test/OrderTest/OrderFactory.cs
@@ -0,0 +1,30 @@
+﻿using DevInSales.Context;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DevInSales.Test.OrderTest
+{
+    internal class OrderFactory : WebApplicationFactory<Program>
+    {
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            var root = new InMemoryDatabaseRoot();
+
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(sp =>
+                {
+                    return new DbContextOptionsBuilder<SqlContext>()
+                        .UseInMemoryDatabase("OrderTestDatabase", root)
+                        .UseApplicationServiceProvider(sp)
+                        .Options;
+                });
+            });
+
+            return base.CreateHost(builder);
+        }
+    }
+}
