57486f2657e4716ee024a08e5312da67c88e5530
Vinícius <vinigsouza.bnu@gmail.com>
2022-06-19 22:37:46 -0300

    Teste de permissionamento GetBuyId - order

--- DevInSales.Test/OrderTest/OrderIntegrationTest.cs
+++ DevInSales.Test/OrderTest/OrderIntegrationTest.cs
@@ -66,5 +66,42 @@ namespace DevInSales.Test.OrderTest
             Assert.That(responseGerente.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
             Assert.That(responseUsuario.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
         }
+
+        [Test]
+        public async Task PermissionamentoGetBuyId()
+        {
+            OrderFactory factory = new OrderFactory();
+            var client = factory.CreateClient();
+            var clientAdmin = factory.CreateClient();
+            var clientGerente = factory.CreateClient();
+            var clientUsuario = factory.CreateClient();
+
+            // given
+
+            var tokenAdmin = TokenGenerator.GenerateToken(Security.EProfileType.Admin);
+            var tokenGerente = TokenGenerator.GenerateToken(Security.EProfileType.Gerente);
+            var tokenUsuario = TokenGenerator.GenerateToken(Security.EProfileType.Usuario);
+
+            clientAdmin.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenAdmin);
+            clientGerente.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenGerente);
+            clientUsuario.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenUsuario);
+
+            // when
+
+            var response = await client.GetAsync("/api/order/user/1/buy");
+            var responseAdmin = await clientAdmin.GetAsync("/api/order/user/1/buy");
+            var responseGerente = await clientGerente.GetAsync("/api/order/user/1/buy");
+            var responseUsuario = await clientUsuario.GetAsync("/api/order/user/1/buy");
+
+            // then
+
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+            Assert.That(responseAdmin.StatusCode, Is.Not.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+            Assert.That(responseAdmin.StatusCode, Is.Not.EqualTo(System.Net.HttpStatusCode.Forbidden));
+            Assert.That(responseGerente.StatusCode, Is.Not.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+            Assert.That(responseGerente.StatusCode, Is.Not.EqualTo(System.Net.HttpStatusCode.Forbidden));
+            Assert.That(responseUsuario.StatusCode, Is.Not.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+            Assert.That(responseUsuario.StatusCode, Is.Not.EqualTo(System.Net.HttpStatusCode.Forbidden));
+        }
     }
 }
